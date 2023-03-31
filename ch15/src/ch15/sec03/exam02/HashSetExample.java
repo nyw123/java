947a31d62294511241660eb012ce660367fd7adb
nyw <vpfa2@naver.com>
2023-03-31 10:44:02 +0900

    add equals hashcode

--- ch15/src/ch15/sec03/exam02/HashSetExample.java
+++ ch15/src/ch15/sec03/exam02/HashSetExample.java
@@ -1,17 +1,28 @@
 package ch15.sec03.exam02;
 
 import java.util.*;
-	
+
 public class HashSetExample {
 	public static void main(String[] args) {
-		//HashSet 컬렉션 생성
+		// HashSet 컬렉션 생성
 		Set<Member> set = new HashSet<Member>();
 
-		//Member 객체 저장
+		Member m1 = new Member("가가가", 10);
+		System.out.println(m1.hashCode());
+		set.add(m1);
+		Member m2 = new Member("가가가", 10);
+		System.out.println(m2.hashCode());
+
+		set.add(m2);
+		System.out.println(m1.equals(m2));
+
+		// Member 객체 저장
 		set.add(new Member("홍길동", 30));
 		set.add(new Member("홍길동", 30));
 
-		//저장된 객체 수 출력
+		// 저장된 객체 수 출력
 		System.out.println("총 객체 수 : " + set.size());
+		for (Member m : set)
+			System.out.println(m.name + m.age);
 	}
 }
